bc33e618c52abd8beb19e450e589ab3d17aa8480
Adio damilare <adiodamilare44@gmail.com>
2024-05-21 02:44:49 +0100

    commit

--- src/Route.js
+++ src/Route.js
@@ -3,4 +3,5 @@ const {CreateState, GetStates,CreateArea,GetArea}=require("./controller")
 
 Router.route("/states").post(CreateState).get(GetStates)
 Router.route("/areas").post(CreateArea).get(GetArea)
+Router.route("/areas/:state").get(GetArea)
 module.exports=Router
